46135c811dabce5e6a86be6f12987c60dd58bcad
allexfree <allexfree@yandex.ru>
2019-02-07 00:39:13 +0300

    tablet version

--- src/js/common.js
+++ src/js/common.js
@@ -8,7 +8,21 @@ $(document).ready(function () {
     dotsClass: 'reviews__slider-pagination',
     arrows: false,
     centerMode: true,
-    variableWidth: true
+    variableWidth: true,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 763,
+        settings: {
+          arrows: true,
+          appendArrows: $('.reviews__slider'),
+          prevArrow: '<button class="reviews__slider-btn" type="button" aria-label="Предыдущий слайдер"></button>',
+          nextArrow: '<button class="reviews__slider-btn" type="button" aria-label="Следующий слайдер"></button>',
+          //centerMode: true,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
 });
