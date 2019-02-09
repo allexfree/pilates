a1b93a80ec9c29b5b4cbc46d1bf0036f0a1e619b
allexfree <allexfree@yandex.ru>
2019-02-09 22:52:20 +0300

    desktop version, menu

--- src/js/common.js
+++ src/js/common.js
@@ -2,6 +2,43 @@
 
 $(document).ready(function () {
 
+  $(window).resize(function() {
+    console.log($('.page-header').height());
+    $('.main-menu').css('display', 'none');
+    $('.main-menu').css('opacity', '0');
+  });
+
+  $('.menu-burger').click(function() {
+    $('.main-menu').css('display', 'flex');
+    $('.main-menu').offset({
+      top: 0,
+    });
+    $('.main-menu').fadeTo(100, 1);
+  });
+
+  $('.main-menu__btn-close').click(function() {
+    if ($(window).width() < 764) {
+      $('.main-menu').offset({
+        top: - $('.page-header').outerHeight()
+      });
+    } else if ($(window).width() === 764 || $(window).width() < 1366) {
+      $('.main-menu').offset({
+        top: - $('.page-header').outerHeight()
+      });
+    } else {
+      $('.main-menu').offset({
+        top: - $('.page-header').outerHeight()
+      });
+    }
+    $('.main-menu').fadeTo(100, 0);
+  });
+
+  $('.main-menu__link').click(function() {
+    $('html, body').stop().animate({
+    scrollTop: $(this.hash).offset().top
+    }, 1000);
+  })
+
   $('.reviews__slider').slick({
     mobileFirst: true,
     dots: true,
@@ -16,8 +53,8 @@ $(document).ready(function () {
         settings: {
           arrows: true,
           appendArrows: $('.reviews__slider'),
-          prevArrow: '<button class="reviews__slider-btn" type="button" aria-label="Предыдущий слайдер"></button>',
-          nextArrow: '<button class="reviews__slider-btn" type="button" aria-label="Следующий слайдер"></button>',
+          prevArrow: '<button class="reviews__slider-btn" type="button" aria-label="Предыдущий слайд"></button>',
+          nextArrow: '<button class="reviews__slider-btn" type="button" aria-label="Следующий слайд"></button>',
           //centerMode: true,
           slidesToShow: 1
         }
